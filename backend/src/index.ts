416389fb1e6a300289b9ba57e0649c8b3427da2d
responsetime <aayush.bhattacharjee2002@gmail.com>
2025-01-05 14:09:17 +0530

    created createRoom route and implemented socket io on frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,36 @@
+import { randomUUID } from "crypto";
 import express from "express";
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
-
+import cors from 'cors'
 const app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer);
-const socket_map: Map<string, Socket> = new Map()
-
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST"],
+  credentials: true
+}))
 type room_info = {
-  playingVideo_url: string,
+  playingVideoUrl: string,
   room_name: string,
-  currentPlayingTime: string,
+  currentPlayingTime: number,
   room_size: number,
+  room_id: string,
 }
 
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173"
+  }
+});
+const socket_map: Map<string, Socket> = new Map()
 const room_map: Map<string, room_info> = new Map()
+
+
 
 io.on("connection", (socket) => {
   socket_map.set(socket.id, socket)
@@ -36,7 +52,7 @@ io.on("connection", (socket) => {
     }
     room_map.set(roomId, {
       ...current,
-      playingVideo_url: videoURL,
+      playingVideoUrl: videoURL,
     })
     socket.to(current.room_name).emit("updateVideoClient", videoURL)
   })
@@ -52,13 +68,12 @@ app.post("/joinRoom", (req, res) => {
    *
    * This route retrieves the `socket_id` and `room_id` from the request body, finds the associated
    * socket in the `socket_map`, and makes it join the specific room.
-   * Finally, it responds with a JSON object indicating success.
+   * Finally, it responds with a JSON object of the room.
    */
   try {
-
+    console.log(req.body)
     const user_socket_id = req.body.socket_id;
     const room_wanted = req.body.room_id;
-
     const room = room_map.get(room_wanted);
     const user_socket = socket_map.get(user_socket_id);
 
@@ -69,14 +84,49 @@ app.post("/joinRoom", (req, res) => {
       res.status(200).json({ Error: error });
       return
     }
-
+    room.room_size += 1
     user_socket.join(room.room_name);
+    console.log(user_socket.rooms)
+    res.json({ room });
     syncTime(room)
-    res.json({ Success: true });
   } catch (e) {
     res.status(500).json({ Error: "Internal Server Error" })
   }
 })
 
+app.post("/createRoom", (req, res) => {
+  /**
+   * Handles a POST request to join watch party room.
+   *
+   * This route retrieves the `socket_id`, `room_name`, and `videoURL` from the request body, creates the room, finds the associated
+   * socket in the `socket_map`, and makes it join the specific room.
+   * Finally, it responds with a JSON object of the Generated Room.
+   */
+  try {
+    console.log(req.body)
+    const user_socket_id = req.body.socket_id;
+    const videoURL = req.body.videoURL;
+    const room_name = req.body.room_name;
+    const roomId = randomUUID()
+    const generated_room: room_info = {
+      playingVideoUrl: videoURL,
+      room_name: room_name,
+      currentPlayingTime: 0.0,
+      room_size: 1,
+      room_id: roomId
+    }
+    room_map.set(roomId, generated_room)
+    const user_socket = socket_map.get(user_socket_id)
+    if (!user_socket) {
+      res.status(500).json({ Error: "User socket not found" })
+      return
+    }
+    user_socket.join(generated_room.room_name)
+    console.log(user_socket.rooms)
+    res.status(200).json({ generated_room })
+  } catch (e) {
+    res.status(500).json({ Error: "Internal Server Error" })
+  }
+})
 
 httpServer.listen(3000);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,62 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import { io } from 'socket.io-client'
 import './App.css'
 
 function App() {
+  const [socketId, setSocketId] = useState<string>()
+  useEffect(() => {
+    const socket = io("http://localhost:3000")
+    socket.on("connect", () => {
+      console.log(socket.id)
+      setSocketId(socket.id)
+    })
+    return () => {
+      socket.disconnect()
+    }
+  }, [])
+  const [room, setRoom] = useState({
+    "room_name": "",
+    "videoURL": ""
+  })
 
+  const handleInputs = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRoom((prevroom) => ({
+      ...prevroom,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const handleCreate = async () => {
+    const res = await fetch("http://localhost:3000/createRoom", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ ...room, socket_id: socketId })
+    })
+    const response = await res.json()
+    console.log(response)
+  }
+  const [roomId, setRoomId] = useState<string>()
+  const handleJoin = async () => {
+    const res = await fetch("http://localhost:3000/joinRoom", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ room_id: roomId, socket_id: socketId })
+    })
+    const response = await res.json()
+    console.log(response)
+  }
   return (
     <>
-      hello world !!!
+      <input type="text" name='room_name' onChange={handleInputs} />
+      <input type="url" name="videoURL" onChange={handleInputs} />
+      <button onClick={handleCreate}>Create Room</button>
+
+      <input type="text" name="room_id" onChange={(e) => { setRoomId(e.target.value) }} />
+      <button onClick={handleJoin}>Join Room</button>
     </>
   )
 }
